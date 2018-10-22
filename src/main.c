59ab92dc55919571869241233a034b56e27cf598
Miguel Angel Bermeo Ayerbe <bermeom@javeriana.edu.co>
2018-10-22 19:17:21 +0200

    It is done

--- src/main.c
+++ src/main.c
@@ -6,9 +6,6 @@
 
 #include "conway.h"
 
-struct GameOfLife{
-
-};
 
 int getPos(int i, int j, int m){
   return m*i+j;
@@ -16,6 +13,7 @@ int getPos(int i, int j, int m){
 
 
 void printBoard(bool *board,int *n,int *m){
+  move(0,0);
   for (int i = 0; i < *n; i++){
     for (int j = 0; j < *m; j++){
       mvaddch(i,j,board[getPos(i,j,*m)]?( 'o'):' ');   // position 10 10, put an X
@@ -28,18 +26,15 @@ int y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 bool isValidPos(int i,int j, int *n, int *m){
   return 0 <= i && i < *n && 0 <= j && j < *m;
-
 }
 
 bool isAlive(bool *board, int *n, int *m, int i, int j){
   int aliveCounter = 0;
-
   for(int k = 0; k < 8; k++){
     if(isValidPos(i+x[k],j+y[k],n,m)){
       aliveCounter += (int)board[getPos(i+x[k],j+y[k],*m)] ;
     }
   }
-
   if (board[getPos(i,j,*m)] && 1 < aliveCounter && aliveCounter < 4){
     return true;
   }
@@ -50,47 +45,52 @@ bool isAlive(bool *board, int *n, int *m, int i, int j){
 
 }
 
-void updateBoard(bool *board,int *n,int *m){
+void updateBoard(bool **board,int *n,int *m){
   bool *newBoard = (bool *) malloc((*n)*(*m)*sizeof(bool));
   for (int i = 0; i < *n; i++){
     for (int j = 0; j < *m; j++){
-       newBoard[getPos(i,j,*m)]=isAlive(board,n,m,i,j);
+       newBoard[getPos(i,j,*m)]=isAlive(*board,n,m,i,j);
     }
   }
-  remove(board);
-  board=newBoard;
+  remove(*board);
+  *board=newBoard;
 }
+
+void setupBlinkerP2(bool *board,int *n,int *m){
+  board[getPos(4,3,*m)] = true;
+  board[getPos(4,4,*m)] = true;
+  board[getPos(4,5,*m)] = true;
+}
+
+void setupBeacon(bool *board,int *n,int *m){
+  board[getPos(2,2,*m)] = true;
+  board[getPos(2,3,*m)] = true;
+  board[getPos(3,2,*m)] = true;
+  board[getPos(5,5,*m)] = true;
+  board[getPos(5,4,*m)] = true;
+  board[getPos(4,5,*m)] = true;
+}
+
 
 int main(){
   initall();    //
 
-  refresh();			/* Print it on to the real screen */
-  // mvaddch(10,100,'X');
-  // mvaddch(20,10,'O');
-  // refresh();			/* Print it on to the real screen */
-
   int n = 10,m = 10;    //n = rows  m = columns
   bool *board = (bool *) malloc(n*m*sizeof(bool));  // Create a vector of n by m
   memset(board,0,n*m);    // Set all tehe vector to 0
-  board[getPos(4,3,m)] = true;
-  board[getPos(4,4,m)] = true;
-  board[getPos(4,5,m)] = true;
-  printBoard(board,&n,&m);
-  refresh();
-    //loop hasta el final
-  for(int f = 0; f < 2; f++){
-    usleep(1000000);
-    updateBoard(board,&n,&m);
+  //setupBlinkerP2(board,&n,&m);
+  setupBeacon(board,&n,&m);
+  
+  for(int f = 0; f < 50; f++){
     printBoard(board,&n,&m);
+    move(m+1,0);attron(A_BOLD);	
+    printw("Iteration: %d",f);attroff(A_BOLD);
     refresh();
+    usleep((int)1e5);
+    updateBoard(&board,&n,&m);
   }
-
-
-
   //*/
-
   getch();			/* Wait for user input */
   enditall();
-
   return 0;
 }
